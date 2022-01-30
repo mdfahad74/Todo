3302ddcf3b38b79522887709980be6262e76c011
mdfahad74 <62789237+mdfahad74@users.noreply.github.com>
2022-01-31 01:40:48 +0530

    minor fixes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-Fahad:Test123@cluster0.pobst.mongodb.net/todolistDB", {useNewUrlParser:true});
+mongoose.connect("mongodb+srv://admin-Fahad:{Key}@cluster0.pobst.mongodb.net/todolistDB", {useNewUrlParser:true});
 
 const itemsSchema = {
     name: String
